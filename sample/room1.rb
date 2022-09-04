00161c7420a6d34367018f2779cca61ce9092b5d
tk3 <tk3inc@gmail.com>
2022-09-04 20:06:33 +0900

    fix room1

--- sample/room1.rb
+++ sample/room1.rb
@@ -3,11 +3,11 @@
 require "curses"
 
 class Room
-  def initialize(height, width, top, left)
+  def initialize(height, width, y, x)
     @height = height
     @width = width
-    @top = top
-    @left = left
+    @y = y
+    @x = x
   end
 
   def box(vertical_char = "|", horizontal_char = "-")
@@ -17,22 +17,22 @@ class Room
 
   def draw
     # top
-    Curses.setpos(@top - 1, @left - 1)
+    Curses.setpos(@y - 1, @x - 1)
     Curses.addstr(@horizontal_char * @width)
 
     # bottom
-    Curses.setpos(@top - 1 + @height - 1, @left - 1)
+    Curses.setpos(@y - 1 + @height - 1, @x - 1)
     Curses.addstr(@horizontal_char * @width)
 
     # left
     (@height - 2).times do |y|
-      Curses.setpos(@top - 1 + y + 1, @left - 1)
+      Curses.setpos(@y - 1 + y + 1, @x - 1)
       Curses.addstr(@vertical_char)
     end
 
     # right
     (@height - 2).times do |x|
-      Curses.setpos(@top - 1 + x + 1, @left - 1 + @width - 1)
+      Curses.setpos(@y - 1 + x + 1, @x - 1 + @width - 1)
       Curses.addstr(@vertical_char)
     end
   end
@@ -42,14 +42,14 @@ end
 if __FILE__ == $0
   Curses.init_screen
 
-  top = 3
-  left = 4
+  x = 3
+  y = 4
   height = 5
   width = 6
 
-  Curses.addstr(">> top=#{top}, left=#{left}, height=#{height}, width=#{width}")
+  Curses.addstr(">> height=#{height}, width=#{width}, y=#{y}, x=#{x}")
 
-  room = Room.new(height, width, top, left)
+  room = Room.new(height, width, y, x)
   room.box
   room.draw
 
