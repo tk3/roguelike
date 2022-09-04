cd862d65897b75cc32a58ef9a90ea61a6a84541e
tk3 <tk3inc@gmail.com>
2022-09-04 19:07:20 +0900

    add player2

---
+++ sample/player2.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require "curses"
+
+Curses.init_screen
+
+s = "@"
+y = Curses.lines / 2
+x = Curses.cols / 2 - (s.length / 2)
+
+Curses.setpos(y, x)
+Curses.addstr(s)
+Curses.refresh
+
+Curses.curs_set(0)
+Curses.noecho
+
+while true do
+  ch = Curses.getch
+
+  case ch
+  when 'h' then
+    Curses.setpos(y, x)
+    Curses.addch(' ')
+
+    x = x - 1
+    Curses.setpos(y, x)
+    Curses.addstr(s)
+    Curses.refresh
+  end
+end
