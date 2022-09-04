296c747dcea2bf044fd8ef2989f007415988b32a
tk3 <tk3inc@gmail.com>
2022-09-04 20:17:04 +0900

    add room2

---
+++ sample/room2.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+
+require "curses"
+
+class Room
+  def initialize(height, width, y, x)
+    @height = height
+    @width = width
+    @y = y
+    @x = x
+  end
+
+  def box(vertical_char = "|", horizontal_char = "-")
+    @vertical_char = vertical_char
+    @horizontal_char = horizontal_char
+  end
+
+  def draw
+    # top
+    Curses.setpos(@y - 1, @x - 1)
+    Curses.addstr(@horizontal_char * @width)
+
+    # bottom
+    Curses.setpos(@y - 1 + @height - 1, @x - 1)
+    Curses.addstr(@horizontal_char * @width)
+
+    # left
+    (@height - 2).times do |y|
+      Curses.setpos(@y - 1 + y + 1, @x - 1)
+      Curses.addstr(@vertical_char)
+    end
+
+    # right
+    (@height - 2).times do |x|
+      Curses.setpos(@y - 1 + x + 1, @x - 1 + @width - 1)
+      Curses.addstr(@vertical_char)
+    end
+
+    # inside
+    (@height - 2).times do |y|
+      (@width - 2).times do |x|
+        Curses.setpos(@y + y, @x + x)
+        Curses.addstr(".")
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  Curses.init_screen
+  Curses.curs_set(0)
+
+  x = 3
+  y = 4
+  height = 5
+  width = 6
+
+  Curses.addstr(">> height=#{height}, width=#{width}, y=#{y}, x=#{x}")
+
+  room = Room.new(height, width, y, x)
+  room.box
+  room.draw
+
+  Curses.refresh
+  Curses.get_char
+end
+
