6516b79969f76d104945b46c3035d94f2c1f3570
tk3 <tk3inc@gmail.com>
2022-09-08 20:21:01 +0900

    update

--- sample/array2d.rb
+++ sample/array2d.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
 
-require "curses"
-
 class Array2d
   attr_reader :max_y, :max_x
   attr_reader :min_y, :min_x
@@ -35,8 +33,18 @@ class Array2d
   end
 
   def insert(arr2d, y, x)
-    (y...(arr2d.max_y + y)).each do |ay|
-      (x...(arr2d.max_x + x)).each do |ax|
+    max_y = if (arr2d.max_y + y) > @max_y
+             @max_y
+           else
+            arr2d.max_y + y
+           end
+    max_x = if (arr2d.max_x + x) > @max_x
+             @max_x
+           else
+            arr2d.max_x + x
+           end
+    (y...max_y).each do |ay|
+      (x...max_x).each do |ax|
         puts ">> #{ay}, #{ax}"
         @grid[ay][ax] = arr2d.grid[ay - y][ax - x]
       end
@@ -46,12 +54,9 @@ end
 
 
 if __FILE__ == $0
-
   arr1 = Array2d.new(5, 5, ".")
-  arr2 = Array2d.new(5, 5, "#")
-  arr1.insert(arr2, 0, 0)
+  arr2 = Array2d.new(4, 4, "#")
+  arr1.insert(arr2, 2, 2)
   arr1.draw
-
-
 end
 
