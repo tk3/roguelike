b36d010ec7941140a87dd6f0f4d3a74dadc7ab05
tk3 <tk3inc@gmail.com>
2022-09-08 20:21:17 +0900

    update

--- sample/game1.rb
+++ sample/game1.rb
@@ -2,44 +2,9 @@
 
 require "curses"
 
-require 'logger'
-
-logger = Logger.new('logfile.log')
-
-
-class Screen
-  attr_reader :max_y, :max_x
-  attr_reader :min_y, :min_x
-
-  def initialize(y, x)
-    @max_y = y
-    @max_x = x
-    @min_y = 0
-    @min_x = 0
-    @grid = Array.new(@max_y) { Array.new(@max_x, '.') }
-  end
-
-  def get(position)
-    @grid[position.y][position.x]
-  end
-
-  def set(position, ch)
-    @grid[position.y][position.x] = ch
-  end
-
-  def draw
-    Curses.erase
-    @grid.each_index do |index|
-      Curses.setpos(index, 0)
-      Curses.addstr(@grid[index].join)
-    end
-  end
-end
-
-def debug(s)
-  Curses.setpos(0, 0)
-  Curses.addstr(s)
-end
+require "./position"
+require "./screen"
+require "./map"
 
 class Actor
   def move(position, input, screen)
@@ -70,7 +35,12 @@ class Player < Actor
   end
 
   def to_ch
-    "@"
+    '@'
+  end
+
+  def draw
+    Curses.setpos(@position.y, @position.x)
+    Curses.addch(self.to_ch)
   end
 end
 
@@ -85,60 +55,44 @@ class GameState
   end
 
   def tick
-    player_prev_position = @player.position.clone
-    prev_tile = @screen.get(player_prev_position)
-
+    @screen.clear
     @screen.set(@player.position, @player.to_ch)
     @screen.draw
+#   @player.draw
 
     while @is_active do
 
+      # player_action
       ch = player_input
+      case ch
+      when 'h' then
+        if (@player.x - 1 >= @screen.min_x) then
+          @player.x = @player.x - 1
+        end
+      when 'q' then
+        @is_active = false
+      end
 
-      player_prev_position = @player.position.clone
-      prev_tile = @screen.get(player_prev_position)
-
-      player_action(ch)
-      break  unless @is_active 
-
-      @screen.set(player_prev_position, prev_tile)
+      @screen.clear
       @screen.set(@player.position, @player.to_ch)
       @screen.draw
+#     @player.draw
     end
   end
 
   def player_input
     Curses.get_char
   end
-
-  def player_action(ch)
-    case ch
-    when 'h' then
-      if (@player.x - 1 >= @screen.min_x) then
-        @player.x = @player.x - 1
-      end
-    when 'q' then
-      @is_active = false
-    end
-  end
 end
 
-class Position
-  attr_accessor :y, :x
-
-  def initialize(y, x)
-    @y = y
-    @x = x
-  end
-end
-
-    logger << "add message"
 
 if __FILE__ == $0
 
   Curses.init_screen
 
   screen = Screen.new(Curses.lines, Curses.cols)
+  map = Map.new(Curses.lines, Curses.cols)
+  map.generate
   player = Player.new(Position.new(screen.max_y / 2, screen.max_x / 2))
 
   state = GameState.new(screen, player)
