c9ca2e474492c1ec859dc49c2d5a93959dbdd71c
tk3 <tk3inc@gmail.com>
2022-09-08 19:13:47 +0900

    add map

---
+++ sample/map.rb
@@ -0,0 +1,88 @@
+#!/usr/bin/env ruby
+
+require "curses"
+
+module TileType
+  WALL = 1
+  FLOOR = 2
+end
+
+class Map
+  attr_reader :max_y, :max_x
+  attr_reader :min_y, :min_x
+
+  def initialize(y, x)
+    @max_y = y
+    @max_x = x
+    @min_y = 0
+    @min_x = 0
+    @grid = Array.new(@max_y) { Array.new(@max_x, TileType::FLOOR) }
+  end
+
+  def generate
+    (@min_x...@max_x).each do |x|
+      @grid[@min_y][x] = TileType::WALL
+      @grid[@max_y - 1][x] = TileType::WALL
+    end
+
+    (@min_y...@max_y).each do |y|
+      @grid[y][@min_x] = TileType::WALL
+      @grid[y][@max_x - 1] = TileType::WALL
+    end
+
+    r = Random.new()
+    (0...400).each do |i|
+      y = r.rand(@max_y)
+      x = r.rand(@max_x)
+      @grid[y][x] = TileType::WALL
+    end
+  end
+
+  def get(position)
+    @grid[position.y][position.x]
+  end
+
+  def set(position, ch)
+    @grid[position.y][position.x] = ch
+  end
+
+  def clear
+    @grid = Array.new(@max_y) { Array.new(@max_x, ' ') }
+  end
+
+  def draw
+    Curses.erase
+    @grid.each_index do |y|
+      @grid[y].each_index do |x|
+        Curses.setpos(y, x)
+        ch = case @grid[y][x]
+             when TileType::FLOOR
+               "."
+             when TileType::WALL
+               "#"
+             else
+               " "
+             end
+        Curses.addch(ch)
+      end
+    end
+    Curses.refresh
+  end
+
+  def copy
+    tmp = Marshal.dump(self)
+    Marshal.load(tmp)
+  end
+end
+
+if __FILE__ == $0
+
+  Curses.init_screen
+
+  map = Map.new(Curses.lines, Curses.cols)
+# map.generate
+  map.draw
+
+  Curses.get_char
+end
+
